01ef5e025517d266189f3df4f8fa907344e8375f
Ling Tan <lingtan.ca@gmail.com>
2018-10-03 22:29:07 -0400

    add menu

--- A2/Live30Seconds/Assets/Scripts/GameController.cs
+++ A2/Live30Seconds/Assets/Scripts/GameController.cs
@@ -1,18 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
+
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    
+    public Text scoreText;
+    public Text progressText;
+ 
+    
+    private bool gameOver;
+    private bool restart;
+    private int score;
 
     // Use this for initialization
     void Start()
     {
-
+        gameOver = false;
+        restart = false;
+        //progressText.text = "Game Start!";
+        scoreText.text = "Scroe: 0";
+        StartCoroutine(SpawnWaves());
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("Normal");
+            }
+        }
+    }
 
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                progressText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        progressText.text = "Game Over!";
+        gameOver = true;
     }
 }
