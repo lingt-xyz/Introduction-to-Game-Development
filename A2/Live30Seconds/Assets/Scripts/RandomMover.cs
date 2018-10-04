8d6f99d6caa5678755bc3fc9e69727932ed39ef2
Ling Tan <lingtan.ca@gmail.com>
2018-10-04 01:26:12 -0400

    a2

---
+++ A2/Live30Seconds/Assets/Scripts/RandomMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMover : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.rotation = Quaternion.Euler(0.1f, 0.2f, 0.1f);
+    }
+}
