996fc55f83c14619d56e0fe56bc94c5188bada52
Ling Tan <ta_li@encs.concordia.ca>
2018-10-23 20:09:57 -0400

    lab7

---
+++ Lab7/Assets/Scripts/Crate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crate : MonoBehaviour
+{
+    [SerializeField]
+    Color selectedColor;
+    [SerializeField]
+    Color unselectedColor;
+
+    public void SetSelected(bool selected)
+    {
+        GetComponent<Renderer>().material.color = selected ? selectedColor : unselectedColor;
+    }
+}
