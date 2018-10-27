d131534ac0b4faa29507ec030cdd75791633636f
Rui <louisa_zhao@hotmail.com>
2018-10-27 16:34:03 -0400

    test assets

---
+++ A3/Live30Seconds/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,64 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public float moveSpeed;
+	public GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera.transform.localPosition = new Vector3 ( 0, 0, 0 );
+		mainCamera.transform.localRotation = Quaternion.Euler (18, 180, 0);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+	void FixedUpdate()
+	{
+		MoveObj ();
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+            SpeedUp();
+		}
+	}
+	
+	void MoveObj() {		
+		float moveAmount = Time.smoothDeltaTime * moveSpeed;
+		transform.Translate ( 0f, 0f, moveAmount );	
+	}
+
+	void SpeedUp() {
+		moveSpeed = -20f;
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
